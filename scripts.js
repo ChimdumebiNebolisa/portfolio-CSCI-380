b91d30d4c75bf0d07e4388782467aa76c24c030e
Chimdumebi Nebolisa <dumebineb@gmail.com>
2025-09-27 17:26:45 -0500

    website built

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const yearSpan = document.getElementById("year");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+
+  console.log("Portfolio site loaded!");
+});
